ce024f49da5d2bb58a16e5b17fee588d4ee8a545
gymyung <gymyung@163.com>
2014-07-03 20:16:17 +0800

    GymYung 完成登陆界面及业务，主界面修改.

---
+++ src/cn/gzu/libaraymana/Util/Const.java
@@ -0,0 +1,7 @@
+package cn.gzu.libaraymana.Util;
+
+import cn.gzu.libaraymana.domain.User;
+
+public class Const {
+	public static User user;
+}
---
+++ src/cn/gzu/libaraymana/activities/BaseActivity.java
@@ -0,0 +1,46 @@
+package cn.gzu.libaraymana.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View.OnClickListener;
+import android.view.Window;
+
+public abstract class BaseActivity extends Activity implements OnClickListener{
+	/**
+	 * 1加载主布局界面
+	 */
+	protected abstract void setContentViewLayout();
+	
+	/**
+	 * 2找到相关组件
+	 */
+	protected abstract void findViewById();
+	
+	/**
+	 * 3为组件添加事件监听
+	 */
+	protected abstract void setCompanentListener();
+	
+	/**
+	 * 4上线处理逻辑
+	 */
+	protected abstract void loadingDeal();
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		
+		initView();
+	}
+
+	private void initView(){
+		setContentViewLayout();
+		findViewById();
+		setCompanentListener();
+		loadingDeal();
+	}
+	
+	
+}

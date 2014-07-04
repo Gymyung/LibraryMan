af189977c7ab2b971dd6586116517b488c77723d
Administrator <Administrator@Sc-201406191112>
2014-07-04 12:40:37 +0800

    dialog

--- gen/cn/gzu/libaraymana/R.java
+++ gen/cn/gzu/libaraymana/R.java
@@ -50,9 +50,11 @@ public final class R {
         public static final int main_version=0x7f020012;
     }
     public static final class id {
-        public static final int action_settings=0x7f0a001c;
+        public static final int action_settings=0x7f0a001f;
         public static final int bookrefer=0x7f0a0003;
         public static final int btn_beginseek=0x7f0a0008;
+        public static final int btn_cancle=0x7f0a001e;
+        public static final int btn_ensure=0x7f0a001d;
         public static final int button1=0x7f0a0002;
         public static final int button2=0x7f0a0005;
         public static final int edit_password=0x7f0a0007;
@@ -75,6 +77,7 @@ public final class R {
         public static final int main_menuitem_version_ll=0x7f0a0015;
         public static final int main_title=0x7f0a000a;
         public static final int main_welcome=0x7f0a000b;
+        public static final int textView1=0x7f0a001c;
         public static final int title_ir=0x7f0a0001;
         public static final int tv_borrow_refer=0x7f0a0009;
         public static final int tv_check=0x7f0a0004;
@@ -85,6 +88,7 @@ public final class R {
         public static final int activity_main=0x7f030001;
         public static final int dbtest=0x7f030002;
         public static final int enter_activity=0x7f030003;
+        public static final int mydialog=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f090000;
@@ -93,7 +97,9 @@ public final class R {
         public static final int action_settings=0x7f070001;
         public static final int app_name=0x7f070000;
         public static final int bms=0x7f070004;
-        public static final int btn_beginseek=0x7f070020;
+        public static final int btn_beginseek=0x7f070021;
+        public static final int btn_cancle=0x7f070024;
+        public static final int btn_ensure=0x7f070023;
         public static final int btn_menu=0x7f07001b;
         public static final int forget_password=0x7f070003;
         public static final int hello_world=0x7f070002;
@@ -117,8 +123,10 @@ public final class R {
         public static final int tv_bookrefer=0x7f07001c;
         public static final int tv_borrow_refer=0x7f07001d;
         public static final int tv_check=0x7f07001e;
-        public static final int tv_seekinfomation=0x7f07001f;
-        public static final int tv_student_id=0x7f070021;
+        public static final int tv_checkbyauthor=0x7f070025;
+        public static final int tv_seekinfomation=0x7f070020;
+        public static final int tv_student_id=0x7f070022;
+        public static final int tv_title_check=0x7f07001f;
         public static final int user_not_exist=0x7f070015;
         public static final int username_not_null=0x7f070012;
         public static final int username_or_password_error=0x7f070014;
